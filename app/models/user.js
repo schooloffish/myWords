aee3ee4b777e8b547d9e6aa9e44ace4fa19b0232
Leo Liu <LeoLiu@thinkgeo.com>
2015-05-07 17:07:33 +0800

    Make login and signup work

--- app/models/user.js
+++ app/models/user.js
@@ -1,23 +1,23 @@
 var mySql = require('mysql');
 var bcrypt = require('bcrypt-nodejs');
 var Sequelize = require('sequelize');
+var sequelize = new Sequelize('mysql://root:123456@localhost:3306/MyWords');
+var User = sequelize.define('User',
+    {
+        username: {type: Sequelize.STRING, field: 'Username'},
+        password: {type: Sequelize.STRING, field: 'Password'}
+    }, {
+        timestamps: false,
+        classMethods: {
+            generateHash: function (password) {
+                return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+            }
+        },
+        instanceMethods: {
+            validPassword: function (password) {
+                return bcrypt.compareSync(password, this.password);
+            }
+        }, freezeTableName: true
+    });
 
-module.exports = function (sequelize) {
-	var User = sequelize.define('User',
-		{
-			userId: { type: Sequelize.BIGINT, field: 'UserId', autoIncrement: true },
-			username: { type: Sequelize.STRING, field: 'Username' },
-			password: { type: Sequelize.STRING, field: 'Password' }
-		}, { 
-			classMethods: {}, 
-			instanceMethods: {
-				generateHash:function (password) {
-					return bcrypt.hashSync(password,bcrypt.getSaltSync(8),null);
-				},
-				validPassword:function (password) {
-					return bcrypt.compareSync(password,this.password);
-				}
-			}, freezeTableName: true });
-
-	return User;
-};
+module.exports = User;
--- config/passport.js
+++ config/passport.js
@@ -1,62 +1,53 @@
 var LocalStrategy = require('passport-local').Strategy;
-
-var Sequelize=require('sequelize');
-
-var sequelize=new Sequelize('mysql://root:123456@localhost:3306/MyWords');
-
-var User = require('../app/models/user')(sequelize);
+var User = require('../app/models/user');
 
 module.exports = function (passport) {
-	passport.serializeUser(function (user, done) {
-		done(null, user.id);
-	});
+    passport.serializeUser(function (user, done) {
+        done(null, user.id);
+    });
 
-	passport.deserializeUser(function (id, done) {
-		User.findById(id, function (err, user) {
-			done(err, user);
-		});
-	});
+    passport.deserializeUser(function (id, done) {
+        User.find({where: {id: id}}).then(function (user) {
+            done(null, user.dataValues);
+        });
+    });
 
-	passport.use('local-signup', new LocalStrategy({
-		usernameField: 'email',
-		passwordField: 'password',
-		passReqToCallback: true
-	}, function (req, email, password, done) {
-			process.nextTick(function () {
-				User.findOne({ 'local.email': email }, function (err, user) {
-					if (err)
-						return done(err);
+    passport.use('local-signup', new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true
+    }, function (req, email, password, done) {
+        process.nextTick(function () {
+            User.find({where: {username: email}}).then(function (user) {
+                //if (err)
+                //    return done(err);
 
-					if (user) {
-						return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
-					}
-					else {
-						var newUser = new User();
-						newUser.local.email = email;
-						newUser.local.password = newUser.generateHash(password);
+                if (user) {
+                    return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+                }
+                else {
+                    var hashedPassword = User.generateHash(password);
+                    User.create({username: email, password: hashedPassword}).then(function (newUser) {
+                        return done(null, newUser.dataValues);
+                    });
+                }
+            });
+        });
+    }));
 
-						newUser.save(function (err) {
-							if (err) throw err;
-							return done(null, newUser);
-						});
-					}
-				});
-			});
-		}));
-
-	passport.use('local-login', new LocalStrategy({
-		// by default, local strategy uses username and password, we will override with email
-		usernameField: 'email',
-		passwordField: 'password',
-		passReqToCallback: true // allows us to pass back the entire request to the callback
-	}, function (req, email, password, done) {
-			User.findOne({ 'local.email': email }, function (err, user) {
-				if (err)
-					return done(err);
-				if (!user)
-					return done(null, false, req.flash('loginMessage', 'No user found.'));
-				if (!user.validPassword(password))
-					return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
-			});
-		}));
+    passport.use('local-login', new LocalStrategy({
+        // by default, local strategy uses username and password, we will override with email
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true // allows us to pass back the entire request to the callback
+    }, function (req, email, password, done) {
+        User.find({where: {username: email}}).then(function (user) {
+            if (!user)
+                return done(null, false, req.flash('loginMessage', 'No user found.'));
+            if (!User.build(user.dataValues).validPassword(password)) {
+                return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
+            }
+            else return done(null, user.dataValues);
+        });
+    }));
 };
